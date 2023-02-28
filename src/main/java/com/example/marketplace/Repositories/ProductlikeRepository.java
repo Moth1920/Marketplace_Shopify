31aa2a2b00a232c5e479cc5cc300fe5c274a101f
RakiZouari <raki.zouari@esprit.tn>
2023-02-28 13:04:45 +0100

    up

--- src/main/java/com/example/marketplace/Repositories/ProductlikeRepository.java
+++ src/main/java/com/example/marketplace/Repositories/ProductlikeRepository.java
@@ -1,4 +1,14 @@
 package com.example.marketplace.Repositories;
 
-public interface ProductlikeRepository {
+import com.example.marketplace.Entities.Productlike;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+@Repository
+public interface ProductlikeRepository extends CrudRepository<Productlike,Long> {
+
+
 }
+
--- src/main/java/com/example/marketplace/Repositories/UserRepository.java
+++ src/main/java/com/example/marketplace/Repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.marketplace.Repositories;
 
-public interface UserRepository {
+import com.example.marketplace.Entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
 }
