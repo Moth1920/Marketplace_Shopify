e4470968948fd6f55e5879b3591ded8c8a163253
Mariem <mariem.haddad@esprit.tn>
2023-03-14 00:01:35 +0100

    up

--- src/main/java/com/example/marketplace/Entities/Adresse.java
+++ src/main/java/com/example/marketplace/Entities/Adresse.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Adresse implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idA;
     @Column(name="adreese1")
     private String adresse1;
    /* @Column(name="adreese2")
