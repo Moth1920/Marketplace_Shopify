201144e6bac27be339d6a8c81903eb0999da4aee
Mariem <mariem.haddad@esprit.tn>
2023-03-13 23:08:25 +0100

    Update ProduitRESTContoller.java

--- src/main/java/com/example/marketplace/RestControllers/ProduitRESTContoller.java
+++ src/main/java/com/example/marketplace/RestControllers/ProduitRESTContoller.java
@@ -1,45 +1,42 @@
 package com.example.marketplace.RestControllers;
 
+import com.example.marketplace.Entities.Boutique;
 import com.example.marketplace.Entities.Categorie;
 import com.example.marketplace.Entities.Produit;
+import com.example.marketplace.Repositories.ProduitRepository;
 import com.example.marketplace.Services.ProduitService;
 import com.example.marketplace.Services.ProduitServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class ProduitRESTContoller {
-    // @Autowired
-    //CategorieRepository categorieRepository;
-    // @RequestMapping(method= RequestMethod.GET)
-    //public List<Categorie> getAllCategories()
-    //{
-    //  return categorieRepository.findAll();
-    //}
-    //@RequestMapping(value="/{id}",method = RequestMethod.GET)
-    //public Categorie getCategorieById(@PathVariable("id") Long id) {
-    //  return categorieRepository.findById(id).get();
-    //}
-
     @Autowired
     ProduitService produitService;
     @Autowired
     ProduitServiceImpl produitServiceImpl;
 
 
-    @PostMapping("/Create")
-    @ResponseBody
-    public Produit createProduit(@RequestBody Produit produit) {
-        return produitService.saveProduit(produit);
-    }
+
+
+
+
+
     @PostMapping("/CreateCategorie")
     @ResponseBody
     public Categorie createCategorie (@RequestBody Categorie categorie) {
         return produitService.saveCategorie(categorie);
+    }
+    @PostMapping("/CreateBou")
+    @ResponseBody
+    public Boutique createBoutique (@RequestBody Boutique boutique) {
+        return produitService.saveBoutique(boutique);
     }
 
 
@@ -47,6 +44,11 @@ public class ProduitRESTContoller {
     @ResponseBody
     public Produit updateProduit(@RequestBody Produit produit,@PathParam("idProduit") Long idProduit) {
         return produitService.updateProduit(produit);
+    }
+    @PutMapping("/Update/{idbou}")
+    @ResponseBody
+    public Boutique updateBoutique(@RequestBody Boutique boutique,@PathParam("idbou") Long idbou) {
+        return produitService.updateBoutique(boutique);
     }
     @PutMapping("/UpdateCategorie/{idCategorie}")
     @ResponseBody
@@ -68,11 +70,32 @@ public class ProduitRESTContoller {
     public List<Categorie> findAllCategories(){
         return produitServiceImpl.findAllCategories();
     }
-    @PostMapping("/addproducttocat/{idCategorie}")
+    @PostMapping("/addproducttocat/{idCategorie}/{idUser}")
     @ResponseBody
-    public void assignProductToCategorie(@RequestBody Produit produit,@PathVariable("idCategorie") Long idCategorie )
+    public void assignProductToCategorie(@RequestBody Produit produit,@PathVariable("idCategorie") Long idCategorie,@PathVariable("idUser") Long idUser )
     {
-        produitService.affectprodtocat(produit, idCategorie);
-    }}
+        produitService.affectprodtocat(produit, idCategorie, idUser);
+    }
+    @GetMapping("/quantity/{quantity}")
+    public List<Produit> FindByQuantityLessThanEqual(@PathVariable ("quantity") int quantity) {
+        return produitService.findByQuantityLessThanEqual(quantity);
+    }
+
+    @GetMapping("/alert")
+    public String showAlert() {
+        return produitService.showAlert();
+    }
+    @PostMapping("/addcattobou/{idbou}")
+    @ResponseBody
+    public void assignCategorieToBoutique(@RequestBody Categorie categorie,@PathVariable("idbou") Long idbou )
+    {
+        produitService.affectcattobou(categorie, idbou);
+    }
+    @GetMapping("/filter/{minPrixProduit}/{maxPrixProduit}")
+    public List<Produit> filterProduit(@PathVariable ("minPrixProduit")float minPrixProduit, @PathVariable ("maxPrixProduit")float maxPrixProduit) {
+        return produitService.filterProduit(minPrixProduit, maxPrixProduit);
+    }
+}
+
 
 
