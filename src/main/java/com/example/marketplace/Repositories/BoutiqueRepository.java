e333b61bafce467ff7b798dc8ce0f745242faff9
Mariem <mariem.haddad@esprit.tn>
2023-03-13 23:21:24 +0100

    bou

--- src/main/java/com/example/marketplace/Repositories/BoutiqueRepository.java
+++ src/main/java/com/example/marketplace/Repositories/BoutiqueRepository.java
@@ -1,4 +1,10 @@
 package com.example.marketplace.Repositories;
 
-public interface BoutiqueRepository {
+import com.example.marketplace.Entities.Boutique;
+import com.example.marketplace.Entities.Categorie;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoutiqueRepository extends CrudRepository<Boutique, Long> {
 }
