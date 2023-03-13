6fcb6a9a87fe48a2911d9becdbffa918d270a1c0
Mariem <mariem.haddad@esprit.tn>
2023-03-13 23:08:16 +0100

    Update ProduitService.java

--- src/main/java/com/example/marketplace/Services/ProduitService.java
+++ src/main/java/com/example/marketplace/Services/ProduitService.java
@@ -1,30 +1,40 @@
 package com.example.marketplace.Services;
 
+import com.example.marketplace.Entities.Boutique;
 import com.example.marketplace.Entities.Categorie;
 import com.example.marketplace.Entities.Produit;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProduitService {
 
-    void affectprodtocat(Produit produit, Long idCategorie);
+    void affectprodtocat(Produit produit, Long idCategorie, Long idUser);
+
+
+    void affectcattobou(Categorie categorie, Long idbou);
 
     List<Produit> findAllProduits();
     List<Categorie> findAllCategories();
 
+    //void addProd(Produit produit, MultipartFile image);
+    List<Produit> filterProduit(float minPrixProduit, float maxPrixProduit);
 
 
-
-    Produit saveProduit(Produit p);
     Categorie saveCategorie(Categorie c);
+    Boutique saveBoutique(Boutique b);
 
     Produit updateProduit(Produit p);
     Categorie updateCategorie(Categorie c);
+    Boutique updateBoutique(Boutique b);
 
-
+    String showAlert();
+    List<Produit> findByQuantityLessThanEqual(int quantity);
 
     void deleteProduit(Produit p);
 
     void deleteProduitById(Long idProduit);
+
+
 
 }
