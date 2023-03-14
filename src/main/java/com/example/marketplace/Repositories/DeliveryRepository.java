3b3796bdf8af65c115387d68ce139eafbf796f9e
Mohamed-Yermani <mohamed.yermani@esprit.tn>
2023-03-14 03:26:03 +0100

    upp

--- src/main/java/com/example/marketplace/Repositories/DeliveryRepository.java
+++ src/main/java/com/example/marketplace/Repositories/DeliveryRepository.java
@@ -1,4 +1,9 @@
 package com.example.marketplace.Repositories;
 
-public interface DeliveryRepository {
+import com.example.marketplace.Entities.Delivery;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends CrudRepository<Delivery,Long> {
 }
--- src/main/java/com/example/marketplace/Repositories/FactureRepository.java
+++ src/main/java/com/example/marketplace/Repositories/FactureRepository.java
@@ -1,4 +1,10 @@
 package com.example.marketplace.Repositories;
 
-public interface FactureRepository {
+import com.example.marketplace.Entities.Delivery;
+import com.example.marketplace.Entities.Facture;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FactureRepository extends CrudRepository<Facture,Long> {
 }
